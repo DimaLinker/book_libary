e35b1818053d9869c684b987603cf80c8c0de6c7
Dima Linker <daniel.linker@mailbox.org>
2024-03-16 00:23:34 +0100

    Book list

--- frontend/src/components/BookList/BookList.jsx
+++ frontend/src/components/BookList/BookList.jsx
@@ -1,10 +1,25 @@
 import React from 'react'
 import './BookList.css'
+import {useSelector} from "react-redux";
 
 export const BookList = () => {
+    const books = useSelector(state => state.books)
     return (
         <div className="app-block book-list">
             <h2>Book List</h2>
+            {books.length === 0 ? (
+                <p>No books found</p>
+            ) : (
+                <ul>
+                    {books.map((book, i) => (
+                        <li key={book.index}>
+                           <div className="book-info">
+                               {i + 1}. {book.title} by <strong>{book.author}</strong>
+                           </div>
+                        </li>
+                    ))}
+                </ul>
+            )}
         </div>
     )
 }
